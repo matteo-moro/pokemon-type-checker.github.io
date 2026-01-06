65cf933a2af38f68db3dc0d745104bc96ecce270
Matteo Moro <moro.matteorosario@gmail.com>
2026-01-06 23:33:06 +0100

    Simplified getting list after actually understanding how caching is supposed to work (plase github pages work as i expect you to), deleted useless files and updated index.html

---
+++ js-scripts/load-list.js
@@ -0,0 +1,19 @@
+var request = new XMLHttpRequest();
+window.pokemonList = document.getElementById("pokemon-name-datalist");
+
+request.onreadystatechange = function(response)
+{
+    if (request.readyState === 4 && request.status === 200)
+    {
+        var data = JSON.parse(request.responseText);
+        data.pokemonlist.forEach(function(pokemon)
+        {
+            var option = document.createElement("option");
+            option.value = pokemon.name;
+            pokemonList.appendChild(option);
+        })
+    }
+}
+
+request.open("GET", "json-files/svlist.json", true);
+request.send();
